ec4148aca74aaa54f6590501501f933c392b40b9
Mark K <markk@example.com>
2026-01-18 04:34:34 +0100

    Refined NBA player prop math with dynamic matchup scaling and tricode mapping

--- utils/mapping.py
+++ utils/mapping.py
@@ -55,3 +55,19 @@ BASKETBALL_ALIASES = {
     "eastcaro": "eastcarolina",
     "coastalcaro": "coastalcarolina",
 }
+# NBA Tricode Mapping
+NBA_TRICODES = {
+    "ATL": "Atlanta Hawks", "BOS": "Boston Celtics", "BKN": "Brooklyn Nets",
+    "CHA": "Charlotte Hornets", "CHI": "Chicago Bulls", "CLE": "Cleveland Cavaliers",
+    "DAL": "Dallas Mavericks", "DEN": "Denver Nuggets", "DET": "Detroit Pistons",
+    "GSW": "Golden State Warriors", "HOU": "Houston Rockets", "IND": "Indiana Pacers",
+    "LAC": "LA Clippers", "LAL": "Los Angeles Lakers", "MEM": "Memphis Grizzlies",
+    "MIA": "Miami Heat", "MIL": "Milwaukee Bucks", "MIN": "Minnesota Timberwolves",
+    "NOP": "New Orleans Pelicans", "NYK": "New York Knicks", "OKC": "Oklahoma City Thunder",
+    "ORL": "Orlando Magic", "PHI": "Philadelphia 76ers", "PHX": "Phoenix Suns",
+    "POR": "Portland Trail Blazers", "SAC": "Sacramento Kings", "SAS": "San Antonio Spurs",
+    "TOR": "Toronto Raptors", "UTA": "Utah Jazz", "WAS": "Washington Wizards"
+}
+
+def get_nba_team_from_tricode(tricode):
+    return NBA_TRICODES.get(tricode, tricode)
